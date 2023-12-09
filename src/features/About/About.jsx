7f1289f3b89b24bbdb9d54c1bfbe593052cbecc8
Miguel <aries_242015@hotmail.com>
2023-12-09 00:40:31 -0500

    agrega about casi finalizado

--- src/features/About/About.jsx
+++ src/features/About/About.jsx
@@ -2,8 +2,23 @@ import React from "react";
 
 export const About = () => {
   return (
-    <div id="about">
-      <h1>About</h1>
+    <div id="about" className="container_about">
+      <div className="container_presentation_profile container">
+        <div className="presentation">
+          <h2>Hi, I am <br /> Miguel Perez</h2>
+          <p>
+            I am a <span>Full Stack Developer</span>
+          </p>        
+          <p>
+            I'm currently studying systems engineering and I'm passionate about
+            the development of web and mobile applications.
+          </p>
+          <a href="#">CV</a>
+        </div>
+        <div className="profile_image">
+          <img src="./src/assets/logo.png" alt="logo" />
+        </div>
+      </div>
     </div>
   );
 };
