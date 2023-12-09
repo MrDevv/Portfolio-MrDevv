8b7ca9d8b9c304b82b37420e4592c0d4410556e2
Miguel <aries_242015@hotmail.com>
2023-12-09 00:58:31 -0500

    agrega animacion de roles en la seccion de about

--- src/features/About/About.jsx
+++ src/features/About/About.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 
 export const About = () => {
   return (
     <div id="about" className="container_about">
       <div className="container_presentation_profile container">
         <div className="presentation">
-          <h2>Hi, I am <br /> Miguel Perez</h2>
+          <h2>
+            Hi, I am <br /> Miguel Perez
+          </h2>
           <p>
-            I am a <span>Full Stack Developer</span>
-          </p>        
+            I am a{" "}
+            <span>
+              <Typewriter
+                options={{
+                  strings: ["Full Stack Developer", "Android Developer"],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </span>
+          </p>
           <p>
             I'm currently studying systems engineering and I'm passionate about
             the development of web and mobile applications.
