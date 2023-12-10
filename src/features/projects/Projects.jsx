2118e6ecebc5327804e45ae1e04cbf78455e5c8d
Miguel <aries_242015@hotmail.com>
2023-12-10 04:05:56 -0500

    agrega cards projects finalizada

--- src/features/projects/Projects.jsx
+++ src/features/projects/Projects.jsx
@@ -1,19 +1,97 @@
-import React from 'react'
+import React from "react";
 
 export const Projects = () => {
   return (
-    <div id="projects" className='section_projects'>
-        <h3>Projects</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias odit ab ut, maiores, tenetur cupiditate, quod omnis at ratione fuga amet laudantium necessitatibus? Ea quis, odit sint minus recusandae vero.
-        Magnam voluptate voluptates omnis dicta pariatur! Enim ducimus debitis quaerat aperiam accusantium mollitia ab expedita est? Reiciendis cumque cum error nam labore adipisci quisquam sit, culpa dolorem rerum. Voluptatibus, repellat!
-        Itaque adipisci minima rem voluptates? Temporibus omnis odit accusamus accusantium! Nesciunt veniam ullam dolorem hic ad aspernatur corporis quos aut soluta recusandae! Dicta possimus dolores delectus nam sapiente aliquid cum.
-        Eveniet illum iste reiciendis accusamus aliquid? Velit nam doloribus asperiores eius dolor id laudantium ad, nostrum dolores quibusdam mollitia facere quos cumque reprehenderit quae nihil veritatis aspernatur, blanditiis tempore. Ducimus!
-        Soluta pariatur rerum vero laborum, beatae dicta impedit alias esse numquam? Obcaecati maxime quod est. Sint veniam nulla, exercitationem temporibus asperiores praesentium nostrum, explicabo possimus tempora sunt pariatur est doloribus.
-        Reiciendis, facere incidunt. Iste non distinctio eum harum. Doloribus, quisquam? Exercitationem mollitia tempore ab ea. Magnam blanditiis similique aliquid tempore minus deserunt tempora eum ad quae. Doloremque expedita earum dolorem.
-        Quae, error beatae modi impedit laborum doloremque dignissimos! Magnam odio maiores repudiandae, reprehenderit accusantium quis tenetur nihil cum, temporibus nisi placeat dolore illo est soluta explicabo culpa doloremque commodi cumque!
-        Ea sequi quaerat iure magnam sapiente quidem? Hic delectus, ipsa tenetur dolorem corporis reiciendis ea optio architecto sint, dolores temporibus veniam aspernatur consequuntur cupiditate? Possimus ullam deserunt dolorem expedita enim.
-        Laborum, corrupti? Magni rem distinctio aliquid. Quia veritatis quis ea ipsum asperiores dolore temporibus excepturi quasi nulla labore aperiam voluptatibus consequuntur deserunt quo doloribus, illum sapiente explicabo aspernatur sed deleniti?
-        Labore facere odio nemo hic pariatur delectus fugit, dignissimos, temporibus eveniet officiis odit iure maiores excepturi doloremque, aspernatur expedita ea repellat accusamus cumque ut animi ipsam architecto iusto eum. Odit?</p>
+    <div id="projects" className="section_projects">
+      <h3>Projects</h3>
+      <div className="container_projects">
+        <div className="options">
+          <div className="option option_active">All</div>
+          <div className="option">WEB APP'S</div>
+          <div className="option">ANDROID APP'S</div>
+        </div>
+        <div className="projects">
+          <div className="card_project">
+            <img src="https://imgs.search.brave.com/TEi-GIfwu1peQTxwjnmeJupe2DidX4XXvFK5E_RCV0Y/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1QlpERXlOMk5o/TWpndE1qZGhOaTAw/TW1ObExXRTVZVGd0/WkdFNE16TmpNVFJs/TUdFd1hrRXlYa0Zx/Y0dkZVFYVnlORFV5/T1RnM05qZ0AuanBn" alt="" />
+            <div className="technologies">
+              <span className="technology">Docker</span>
+              <span className="technology">AWS</span>
+              <span className="technology">DuckDNS</span>
+              <span className="technology">Eslint</span>
+              <span className="technology">Husky</span>
+              <span className="technology">React Js</span>
+              <span className="technology">MongoDb</span>
+              <span className="technology">Node Js</span>
+              <span className="technology">Redux</span>
+            </div>
+            <div className="description">
+              <div className="title">Trackify</div>
+              <p className="information">Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It Provices a user-friendly interface for employers to keep track of their employees employees employees employees employees</p>
+            </div>
+            <div className="buttons">
+              <a className="view_code" href="#">View Code</a>
+              <a className="view_live" href="#">View Live App</a>
+            </div>
+          </div>
+
+          <div className="card_project">
+            <img src="https://imgs.search.brave.com/TEi-GIfwu1peQTxwjnmeJupe2DidX4XXvFK5E_RCV0Y/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1QlpERXlOMk5o/TWpndE1qZGhOaTAw/TW1ObExXRTVZVGd0/WkdFNE16TmpNVFJs/TUdFd1hrRXlYa0Zx/Y0dkZVFYVnlORFV5/T1RnM05qZ0AuanBn" alt="" />
+            <div className="technologies">
+            <span className="technology">Docker</span>
+              <span className="technology">AWS</span>
+              <span className="technology">DuckDNS</span>
+              <span className="technology">Eslint</span>
+              <span className="technology">Husky</span>
+              <span className="technology">React Js</span>
+              <span className="technology">MongoDb</span>
+              <span className="technology">Node Js</span>
+              <span className="technology">Redux</span>
+            </div>
+            <div className="description">
+              <div className="title">Trackify</div>
+              <p className="information">Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It Provices a user-friendly interface for employers to keep track of their employees</p>
+            </div>
+          </div>
+
+          <div className="card_project">
+            <img src="" alt="" />
+            <div className="technologies">
+            <span className="technology">Docker</span>
+              <span className="technology">AWS</span>
+              <span className="technology">DuckDNS</span>
+              <span className="technology">Eslint</span>
+              <span className="technology">Husky</span>
+              <span className="technology">React Js</span>
+              <span className="technology">MongoDb</span>
+              <span className="technology">Node Js</span>
+              <span className="technology">Redux</span>
+            </div>
+            <div className="description">
+              <div className="title">Trackify</div>
+              <p className="information">Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It Provices a user-friendly interface for employers to keep track of their employees</p>
+            </div>
+          </div>
+
+          <div className="card_project">
+            <img src="" alt="" />
+            <div className="tecnologies">
+            <span className="technology">Docker</span>
+              <span className="technology">AWS</span>
+              <span className="technology">DuckDNS</span>
+              <span className="technology">Eslint</span>
+              <span className="technology">Husky</span>
+              <span className="technology">React Js</span>
+              <span className="technology">MongoDb</span>
+              <span className="technology">Node Js</span>
+              <span className="technology">Redux</span>
+            </div>
+            <div className="description">
+              <div className="title">Trackify</div>
+              <p className="information">Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It Provices a user-friendly interface for employers to keep track of their employees</p>
+            </div>
+          </div>
+        </div>        
+      </div>
     </div>
-  )
-}
+  );
+};
