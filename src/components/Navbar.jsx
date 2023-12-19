40079c4005401c040b7b75bab90cf68efb3ca5f7
Miguel <aries_242015@hotmail.com>
2023-12-19 17:07:38 -0500

    agrega responsive design finalizado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,37 +3,57 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   const [activeSection, setActiveSection] = useState(null);
+  const [activeToggleMenu, setactiveToggleMenu] = useState(false);
 
-  const isActive = (section) => section === activeSection ? "active" : undefined;
+  const isActive = (section) =>
+    section === activeSection ? "active" : undefined;
 
   const handleLinkClick = (section) => {
     setActiveSection(section === activeSection ? null : section);
   };
 
+  console.log(activeToggleMenu);
+
   return (
     <div className="container_navbar">
-      <div className="container_logo_links_github container">
+      <div className="container_logo_links_github container">     
+      <div
+          className="box-shadow"
+          style={activeToggleMenu ? { display: "block" } : { display: "none" }}
+          onClick={()=> setactiveToggleMenu(!activeToggleMenu)}
+        ></div>   
         <div className="logo">
-          <Link to={"/"} onClick={()=> setActiveSection(null)}>MrDevv</Link>
+          <Link to={"/"} onClick={() => setActiveSection(null)}>
+            MrDevv
+          </Link>
         </div>
-        <div className="links">
+        <div className={`links ${activeToggleMenu ? "showLinks" : ""}`}>
           <a
             className={isActive("links")}
-            onClick={() => handleLinkClick('links')}
+            onClick={() => {
+              handleLinkClick("links");
+              setactiveToggleMenu(!activeToggleMenu);
+            }}
             href="#about"
           >
             About
           </a>
           <a
             className={isActive("skills")}
-            onClick={() => handleLinkClick('skills')}
+            onClick={() => {
+              handleLinkClick("skills");
+              setactiveToggleMenu(!activeToggleMenu);
+            }}
             href="#skills"
           >
             Skills
           </a>
           <a
             className={isActive("projects")}
-            onClick={() => handleLinkClick('projects')}
+            onClick={() => {
+              handleLinkClick("projects");
+              setactiveToggleMenu(!activeToggleMenu);
+            }}
             href="#projects"
           >
             Projects
@@ -44,7 +64,24 @@ export const Navbar = () => {
             Github Profile
           </a>
         </div>
-      </div>
+        <div
+          onClick={() => setactiveToggleMenu(!activeToggleMenu)}
+          className="toggle-menu"
+        >
+          <i
+            className="fa-solid fa-bars"
+            style={
+              activeToggleMenu ? { display: "none" } : { display: "block" }
+            }
+          ></i>
+          <i
+            className="fa-solid fa-xmark"
+            style={
+              activeToggleMenu ? { display: "block" } : { display: "none" }
+            }            
+          ></i>
+        </div>
+      </div>      
     </div>
   );
 };
