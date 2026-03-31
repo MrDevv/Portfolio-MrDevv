5a49869975165a507fd5e4d1e60e38d0979a0e3e
Miguel Perez <94268929+MrDevv@users.noreply.github.com>
2026-03-31 17:09:56 -0500

    feat: agregar funcionalidad obtener proyectos (#4)

--- src/features/projects/Projects.jsx
+++ src/features/projects/Projects.jsx
@@ -1,48 +1,65 @@
 import React, { useState } from "react";
 import { projects } from "../../data/constants";
 import { CardProject } from "../../components/CardProject";
+import { useProyectos } from "../../hooks/useProyectos";
+import Swal from "sweetalert2";
 
 export const Projects = () => {
   const [optionSelected, setoptionSelected] = useState("all");
+  const {proyectos, loading, error} = useProyectos();
+
+  console.log({proyectos, loading, error});
+
+  const tipoProyectos = [
+  ...new Set(proyectos.map(p => p.tipo_proyecto.descripcion))
+  ];  
+
+  if (error) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: error.message
+    });
+  }
 
   const isActive = (option) =>
     optionSelected === option ? "option_active" : "";
 
   return (
     <div id="projects" className="section_projects">
-      <h3>Projects</h3>
+      <h3>Proyectos</h3>
       <div className="container_projects">
         <div className="options">
           <div
             onClick={() => setoptionSelected("all")}
             className={`option ${isActive("all")}`}
           >
-            All
+            Todos
           </div>
+
+          {
+            tipoProyectos.map((tipo) => (
+              <div
+                key={tipo}
+                onClick={() => setoptionSelected(tipo)}
+                className={`option ${isActive(tipo)}`}
+              >
+                { tipo }
+              </div>
+            ))
+          }
+        </div>
           <div
-            onClick={() => setoptionSelected("web app")}
-            className={`option ${isActive("web app")}`}
-          >
-            WEB APP'S
-          </div>
-          <div
-            onClick={() => setoptionSelected("android app")}
-            className={`option ${isActive("android app")}`}
-          >
-            ANDROID APP'S
-          </div>
-        </div>        
-          <div                       
           className="projects"
           >
             {optionSelected === "all"
-              ? projects.map((project) => (
-                  <CardProject key={project.id} project={project} />
+              ? proyectos.map((project) => (
+                  <CardProject key={project.proyecto_id} project={project} />
                 ))
-              : projects
-                  .filter((project) => project.category === optionSelected)
+              : proyectos
+                  .filter((project) => project.tipo_proyecto.descripcion === optionSelected)
                   .map((project) => (
-                    <CardProject key={project.id} project={project} />
+                    <CardProject key={project.proyecto_id} project={project} />
                   ))}
           </div>
       </div>
